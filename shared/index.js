9ec3e8b08b0942f0b50036512e1e576e20103ef5
homer0 <leo.apiwan@olapic.com>
2018-01-24 01:03:59 -0300

    Github page

--- shared/index.js
+++
@@ -1,9 +0,0 @@
-const APIClient = require('./apiClient');
-const deferred = require('./deferred');
-const EventsHub = require('./eventsHub');
-
-module.exports = {
-  APIClient,
-  deferred,
-  EventsHub,
-};
